c0be6932e130ce3e3c0f7cbf3db9db108947c8e9
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2024-07-05 20:06:44 +0530

    Time: 353 ms (57.03%) | Memory: 45.2 MB (6.08%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -6,25 +6,25 @@
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         l=[]
-        temp = head.next
-        prev = head
-        ind = 1
-        while temp.next is not None:
-            if temp.val > temp.next.val and temp.val > prev.val:
-                l.append(ind)
-            elif temp.val < temp.next.val and temp.val < prev.val:
-                l.append(ind)
-            ind += 1
-            prev = temp
-            temp = temp.next
+        copy=head.next
+        pre=head.val
+        i=0
+        while copy.next!=None:
+            if copy.val>copy.next.val and copy.val>pre:
+                l.append(i)
+            elif copy.val<copy.next.val and copy.val<pre:
+                l.append(i)
+            i+=1
+            pre=copy.val
+            copy=copy.next
         i=0
         j=1
         a=len(l)-1
         l1=[]
-        print(a)
-        print(l)
+        # print(a)
+        # print(l)
         while a>0:
-            print(l[j]-l[i])
+            # print(l[j]-l[i])
             l1.append(l[j]-l[i])
             i+=1
             j+=1
