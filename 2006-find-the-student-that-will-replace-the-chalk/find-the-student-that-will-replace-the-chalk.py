04ceb75992ba09d73c400ba78ba4f0cfc9c33e5e
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2024-09-02 20:28:27 +0530

    Time: 579 ms (48.81%) | Memory: 29.9 MB (84.17%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,26 +1,9 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         s=sum(chalk)
-        a=sum(chalk)
-        if len(chalk)==1:
-            return 0
-        if s==k:
-            return 0
-        elif s<k:
-            while s<=k:
-                if s==k:
-                    return 0
-                else:
-                    s+=a
-            s-=(a)
-            for i in range(len(chalk)):
-                s+=chalk[i]
-                if s>k:
-                    return i
-        else:
-            # return 0
-            s=0
-            for i in range(len(chalk)):
-                s+=chalk[i]
-                if s>k:
-                    return i
+        remainder=k%s
+        cu=0
+        for i in range(len(chalk)):
+            cu+=chalk[i]
+            if cu>remainder:
+                return i
