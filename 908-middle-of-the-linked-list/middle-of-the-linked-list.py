97d00be120e401d9b99932e093223d6f0e1007b7
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2025-06-13 09:57:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.57%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while(fast != None and fast.next !=None):
+            fast=fast.next.next
+            slow = slow.next
+            if fast == None:
+                return slow
+        return slow
