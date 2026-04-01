2ed91f01c2a559f035ca9c6f1a1d0f1eae00ed87
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2026-04-01 17:08:38 +0530

    Time: 96 ms (74.43%) | Memory: 51.1 MB (81.39%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,15 +1,4 @@
 class Solution {
-    public static boolean isPrime(int n){
-        if(n<=1){
-            return false;
-        }
-        for(int i = 2 ; i*i<=n;i++){
-            if(n%i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
     public int countPrimes(int n) {
         if(n<=2) return 0;
         boolean[] isPrime = new boolean[n];
