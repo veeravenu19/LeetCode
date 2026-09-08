8d0fb6ff7426d13ae4524a91fdd92b02b033dbba
veeravenu19 <143855799+veeravenu19@users.noreply.github.com>
2026-09-08 08:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.87%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.py
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -3,9 +3,10 @@ class Solution:
         if n<=999:
             return 0
         else:
-            c=0
-            for i in range(1000,n+1):
-                a = f"{i:,}"
-                c += a.count(',')
-            return c
-        
+            # c=0
+            # for i in range(1000,n+1):
+            #     a = f"{i:,}"
+            #     c += a.count(',')
+            # return c
+            if n<= 199999:
+                return n-999
